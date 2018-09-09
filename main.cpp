df2fc2a6b5f9a4a5d35971c715f481debe40f2ab
Danieljudi <41111907+Danieljudi@users.noreply.github.com>
2018-09-09 22:08:10 +0200

    for_imiona

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+string imie;
+int liczba;
+int main()
+{
+
+    cout << "Podaj imie:" << endl;
+    cin>> imie;
+    cout<< "Podaj dodatnia liczbe calkowita:";
+    cin>> liczba;
+    for (int i=1;i<=liczba;i++)
+        cout<<i<<"."<<imie<<endl;
+    return 0;
+}
